cd196595bcf372f8a12cf7701e03b78345b2e106
Administrator <Administrator@USER-2017ytghu>
2017-11-28 18:37:23 +0800

    test

---
+++ src/day22/SubScan.java
@@ -0,0 +1,19 @@
+package day22;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+
+public class SubScan {
+
+	public static void main(String[] args) throws IOException {
+		Scanner scn =new Scanner(System.in);
+BufferedReader br =new BufferedReader(new InputStreamReader(System.in,"GBK2312"));
+System.out.println("Please input");
+String str = br.readLine();
+System.out.println("str:"+str);
+	}
+
+}
---
+++ src/day22/test.java
@@ -0,0 +1,12 @@
+package day22;
+
+public class test {
+public static void main(String[] args) {
+	String str1="{\"key\":10}";
+	String str2="{\"key\":\"Hello\"}";
+	String str3="{\"key\":{\"InnerKey\":World\"}}";
+	
+	
+	JSONObject obj1= new JSONObject(str2);
+}
+}
